9d4b2d05e7f9e2be0faab7544024cb1aeb495617
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-06-17 16:24:40 +0300

    Update task7.py

--- src/task7.py
+++ src/task7.py
@@ -2,8 +2,9 @@ a = int(input("a = "))
 b = int(input("b = "))
 c = int(input("c = "))
 p = (a + b + c) / 2
-s = (p * (p - a) * (p - b) * (p-c)) ** (0.5)
+s = (p * (p - a) * (p - b) * (p - c)) ** (0.5)
 if a + b > c and a + c > b and b + c > a:
     print("Площадь треугольника равно", s)
 else:
     print("Неверные данные")
+    
