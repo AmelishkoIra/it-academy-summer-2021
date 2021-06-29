e1fd84337916837391cb8f64dc7fe9838361766e
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-06-29 16:39:33 +0300

    Update task1.py

--- src/task1.py
+++ src/task1.py
@@ -2,14 +2,14 @@
 # но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz,
 # а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz.
 
-str_ = []
+list_numbers = []
 for i in range(1, 101):
     if int(i) % 3 == 0 and int(i) % 5 == 0:
-        str_.append("FizzBuzz")
+        list_numbers.append("FizzBuzz")
     elif int(i) % 3 == 0:
-        str_.append("Fizz")
+        list_numbers.append("Fizz")
     elif int(i) % 5 == 0:
-        str_.append("Buzz")
+        list_numbers.append("Buzz")
     else:
-        str_.append(i)
-print(str_)
+        list_numbers.append(i)
+print(list_numbers)
