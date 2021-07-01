18d03a580ecc8084916b3fd8d82886c1b115bf65
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-07-01 13:48:49 +0300

    Add files via upload

--- src/task5.py
+++ src/task5.py
@@ -1,11 +1,23 @@
-# Дан список. Выведите те его элементы, которые встречаются
-# в списке только один раз. Элементы нужно выводить
-# в том порядке, в котором они встречаются в списке
+# Каждый из N школьников некоторой школы знает Mi языков.
+# Определите, какие языки знают все школьники и языки,
+# которые знает хотя бы один из школьников.
 
-lst = [1, 2, 4, 5, 7, 7, 4]
-lst_new = ""
-for i in lst:
-    if lst.count(i) <= 1:
-        if str(i) not in lst_new:
-            lst_new += str(i)
-print(lst_new)
+children = int(input("Количество школьников: "))
+languages = []
+for i in range(children):
+    if i <= children:
+        languages_num = int(input("Количество языков школьника: "))
+        for j in range(languages_num):
+            if j <= languages_num:
+                languag = input("Введите языки: ")
+                languages.append(languag)
+set_languages = set(languages)
+languages_all = []
+for i in languages:
+    if languages.count(i) == children:
+        if i not in languages_all:
+            languages_all.append(i)
+print("Кол-во языков, которые знают все школьники:", len(languages_all))
+print("Языки, которые знают все школьники: ", languages_all)
+print("Кол-во языков,которые знает хотя бы один школьник: ", len(set_languages))
+print("Языки, которые знает хотя бы один школьник: ", set_languages)
