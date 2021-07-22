f4a60b68e3ff2e9f6916ad19ab6e8e381302d56d
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-07-22 16:53:33 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -2,13 +2,14 @@
 Вводится число, найти его максимальный делитель, являющийся степенью двойки.
 10(2) 16(16), 12(4).
 """
+def max_divisor(number):
+    exponent = 0
+    while 2 ** exponent <= number:
+        if number % (2 ** exponent) == 0:
+            divisor = (2 ** exponent)
+            exponent = exponent + 1
+        else:
+            break
+    print("Наибольший общий делитель: ", divisor)
 
-number = int(input("ВВедите число: "))
-exponent = 0
-while 2 ** exponent <= number:
-    if number % (2 ** exponent) == 0:
-        divisor = (2 ** exponent)
-        exponent = exponent + 1
-    else:
-        break
-print("Наибольший общий делитель: ", divisor)
+max_divisor(10)
