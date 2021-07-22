ea1e6d3b24613e089287fe846fdb8385753a642f
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-07-22 16:51:54 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,33 +4,39 @@
 список сворачивает.
 """
 
-
 def get_ranges(number_list):
 
     zone_one = list(filter(lambda x: x <= 4, number_list))
     zone_two = list(filter(lambda x: x >= 5 and x <= 6, number_list))
     zone_three = list(filter(lambda x: x >= 7 and x <= 9, number_list))
     zone_four = list(filter(lambda x: x >= 10, number_list))
+    a = []
+
     if len(zone_one) != 0:
         if len(zone_one) == 1:
-            print(zone_one)
+            a = a + zone_one
         else:
-            print(min(zone_one), "-", max(zone_one))
+            one = [f"{min(zone_one)} - {max(zone_one)}"]
+            a = a + one
     if len(zone_two) != 0:
         if len(zone_two) == 1:
-            print(zone_two)
+            a = a + zone_two
         else:
-            print(min(zone_two)), "-", max(zone_two)
+            two = [f"{min(zone_two)} - {max(zone_two)}"]
+            a = a + two
     if len(zone_three) != 0:
         if len(zone_three) == 1:
-            print(zone_three)
+            a = a + zone_three
         else:
+            three = [f"{min(zone_three)} - {max(zone_three)}"]
             print(min(zone_three), "-", max(zone_three))
     if len(zone_four) != 0:
         if len(zone_four) == 1:
-            print(zone_four)
+            a = a + zone_four
         else:
-            print(min(zone_four), "-", max(zone_four))
+            four = [f"{min(zone_four)} - {max(zone_four)}"]
+            a = a + four
+    print(a)
 
 
-get_ranges([2, 4, 10])
+get_ranges([1, 2, 3, 5, 6, 7, 10])
