c0ebdb8e9737ff3652e2683075181c307a2e68d1
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-10 16:47:29 +0300

    Update test hw2-9.py

--- src/test hw2-9.py
+++ src/test hw2-9.py
@@ -1,6 +1,6 @@
-import unittest
 import ddt
 import task1_hw2
+import unittest
 
 
 @ddt.ddt
@@ -10,9 +10,7 @@ class Test_area_triangle(unittest.TestCase):
         (3, 4, 5, "Площадь треугольника равно 6.0"),
         (1, 4, 5, "Неверные данные"),
         (5, 5, 5, "Площадь треугольника равно 10.825317547305483")
-
     )
-
     @ddt.unpack
     def test_value(self, a, b, c, expected):
         result = task1_hw2.area_triangle(a, b, c)
@@ -23,7 +21,6 @@ class Test_area_triangle(unittest.TestCase):
         ({5}, [5], [5], TypeError),
         ("5", "9", "8", TypeError),
     )
-
     @ddt.unpack
     def test_errors(self, a, b, c, expected):
         with self.assertRaises(expected):
