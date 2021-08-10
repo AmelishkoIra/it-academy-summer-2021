5848639744b45fcd7e90553a4aeb9e693f8d6885
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-10 16:44:28 +0300

    Update test hw2-3.py

--- src/test hw2-3.py
+++ src/test hw2-3.py
@@ -1,6 +1,6 @@
-import unittest
 import ddt
 import task1_hw2
+import unittest
 
 
 @ddt.ddt
@@ -11,12 +11,10 @@ class Test_func_sum(unittest.TestCase):
         (5, 8, 12, (60, 96)),
         (10, 4, 67, (672, 68)),
     )
-
     @ddt.unpack
     def test_value(self, price_rub, price_kop, product, final_price):
         result = task1_hw2.func_sum(price_rub, price_kop, product)
         self.assertEqual(result, final_price)
-
 
     @ddt.data(
         (10, 3, [3], TypeError),
@@ -24,7 +22,6 @@ class Test_func_sum(unittest.TestCase):
         ([2], [4], [67], TypeError),
         ({2}, {9}, {89}, TypeError),
     )
-
     @ddt.unpack
     def test_errors(self, price_rub, price_kop, product, final_price):
         with self.assertRaises(final_price):
