8c278f7127a70c49a9a20b4105e2fc627c8e73ef
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-10 16:49:50 +0300

    Update test hw4-1.py

--- src/test hw4-1.py
+++ src/test hw4-1.py
@@ -1,6 +1,7 @@
-import unittest
 import ddt
 import task1_hw4
+import unittest
+
 
 @ddt.ddt
 class Test_different_words(unittest.TestCase):
@@ -10,7 +11,6 @@ class Test_different_words(unittest.TestCase):
         ("Ron is a boy", 4),
         ("well well well", 1)
     )
-
     @ddt.unpack
     def test_value(self, input_data, expected):
         result = task1_hw4.counting_different_words(input_data)
@@ -22,7 +22,6 @@ class Test_different_words(unittest.TestCase):
         ({1, 2, 3}, AttributeError),
         ({1:5}, AttributeError),
     )
-
     @ddt.unpack
     def test_error(self, input_data, expected):
         with self.assertRaises(expected):
