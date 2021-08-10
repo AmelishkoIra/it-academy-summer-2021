9aa460054409b14d043ce0c0b7fba87e9253657c
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-10 16:38:52 +0300

    Update test hw2-12.py

--- src/test hw2-12.py
+++ src/test hw2-12.py
@@ -1,6 +1,6 @@
-import unittest
 import ddt
 import task1_hw2
+import unittest
 
 
 @ddt.ddt
@@ -10,7 +10,6 @@ class Test_bus(unittest.TestCase):
         (2, 4, 5, "необходимо 2 автобусa"),
         (9, 1, 5, "поездка не состоится"),
         (9, 6, 5, "необходимо 3 автобусa"),
-
     )
     @ddt.unpack
     def test_value(self, input_data1, input_data2, input_data3, expected):
@@ -23,8 +22,8 @@ class Test_bus(unittest.TestCase):
         ({5}, [3], [8], TypeError),
         (3, 7, 0, ZeroDivisionError),
     )
-
     @ddt.unpack
     def test_errors(self, input_data1, input_data2, input_data3, expected):
         with self.assertRaises(expected):
             task1_hw2.bus(input_data1, input_data2, input_data3)
+            
