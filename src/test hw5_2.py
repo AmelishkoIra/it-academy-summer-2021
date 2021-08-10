b1be4c77bf450a9937ad44134ac3c9ffa4a22a61
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-10 16:51:58 +0300

    Update test hw5_2.py

--- src/test hw5_2.py
+++ src/test hw5_2.py
@@ -1,6 +1,7 @@
-import unittest
 import ddt
 import task1_hw5
+import unittest
+
 
 @ddt.ddt
 class Test_input_number(unittest.TestCase):
@@ -10,7 +11,6 @@ class Test_input_number(unittest.TestCase):
         (45, 32),
         (999, 1024),
     )
-
     @ddt.unpack
     def test_value(self, input_data, expected):
         result = task1_hw5.input_number(input_data)
@@ -22,7 +22,6 @@ class Test_input_number(unittest.TestCase):
         ({3:2}, TypeError),
         ("32", TypeError),
     )
-
     @ddt.unpack
     def test_errors(self, input_data, expected):
         with self.assertRaises(expected):
