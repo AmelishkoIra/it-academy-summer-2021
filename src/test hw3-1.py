34fb3adfc392805ab4a52262a8408476c00ea1f0
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-20 12:44:47 +0300

    Update test hw3-1.py

--- src/test hw3-1.py
+++ src/test hw3-1.py
@@ -1,10 +1,17 @@
+"""
+тест на функцию fizzbuzz, которая печатает цифры от 1 до 100,
+но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный
+5 пишет Buzz, а вместо чисел одновременно кратных 
+и 3 и 5 - FizzBuzz.
+"""
+
 import ddt
 import task1_hw3
 import unittest
 
 
 @ddt.ddt
-class Test_fizzbuzz(unittest.TestCase):
+class TestFizzbuzz(unittest.TestCase):
 
     @ddt.data(
         (15, "FizzBuzz"),
@@ -15,6 +22,7 @@ class Test_fizzbuzz(unittest.TestCase):
     )
     @ddt.unpack
     def test_1(self, input_data, expected):
+        """позитивный тест"""
         result = task1_hw3.fizzbuzz(input_data)
         self.assertEqual(result, expected)
 
@@ -27,5 +35,6 @@ class Test_fizzbuzz(unittest.TestCase):
     )
     @ddt.unpack
     def test_2(self, input_data, expected):
+        """негативный тест"""
         with self.assertRaises(expected):
             task1_hw3.fizzbuzz(input_data)
