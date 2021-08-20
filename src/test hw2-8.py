40a0e9a698ec3a56c36ac8f97e84e63ad2e0a515
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-20 12:37:26 +0300

    Update test hw2-8.py

--- src/test hw2-8.py
+++ src/test hw2-8.py
@@ -1,10 +1,15 @@
+"""
+тест на функцию palindrom, которая определяет,
+является ли число палиндромом.
+"""
+
 import ddt
 import task1_hw2
 import unittest
 
 
 @ddt.ddt
-class Test_palindrom(unittest.TestCase):
+class TestPalindrom(unittest.TestCase):
 
     @ddt.data(
         (777, True),
@@ -16,5 +21,6 @@ class Test_palindrom(unittest.TestCase):
     )
     @ddt.unpack
     def test_value(self, input_data, expected):
+        """позитивный тест"""
         result = task1_hw2.palindrom(input_data)
         self.assertEqual(result, expected)
