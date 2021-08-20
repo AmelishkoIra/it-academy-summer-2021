7ff4f73184aaf19426326b012c3f8971482640cb
AmelishkoIra <85646096+AmelishkoIra@users.noreply.github.com>
2021-08-20 12:07:58 +0300

    Update test hw2-11.py

--- src/test hw2-11.py
+++ src/test hw2-11.py
@@ -1,3 +1,7 @@
+"""
+тест на функцию round_number, которая определяет,
+сколько кругляшей в числе. 
+"""
 import ddt
 import task1_hw2
 import unittest
@@ -14,9 +18,11 @@ class Test_round_number(unittest.TestCase):
     )
     @ddt.unpack
     def test_value(self, input_data, expected):
+        """позитивный тест"""
         result = task1_hw2.round_numbers(input_data)
         self.assertEqual(result, expected)
 
     def test_errors(self):
+        """негативный тест"""
         with self.assertRaises(TypeError):
             task1_hw2.round_numbers(444)
